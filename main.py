bae326ccc64a5f19eeb57582aec7d4ea5b37cb62
Rian-Averilla <averilla.ads@obmontessori.edu.ph>
2026-09-15 08:02:59 +0800

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+from pyscript import display
+
+fullname = 'Adriana Averilla' #string
+age = 15 #integer
+height152 = 152.4 #float
+countries = ["Japan", "Italy", "China"] #list
+student_type = False #boolean
+dict = {'Favorite Color': 'Blue', 'car_brand': 'Toyota', 'shoe_Size': 8, 'Best friends': 'Georgina/Ray/Ren/Ciara'} #dictionary
+_fruits = ['Apples', 'Grapes', 'Oranges', 'Blueberries', 'Cherries'] #list
+week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday') #tuple
+
+
+display(f'Name: {fullname}') 
+display(f'Age: {age}') 
+display(f'Height: {height152} cm')
+display(f'Countries I want to visit: {countries}')
+display(f'Am I a new student: {student_type}')
+display(f'My favorites: {dict}')
+display(f'Favorite fruits: {_fruits}')
+display(f'Days of the week: {week}')
